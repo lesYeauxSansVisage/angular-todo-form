a173995e4f8b341ca0ccb90dd024ea0a20c2e624
Gen <genesishawkeye42@gmail.com>
2022-10-24 21:14:28 -0300

    added a mock file for todos

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,28 @@
+import Todo from './interfaces/Todo';
+
+export const mockTodos: Todo[] = [
+  {
+    id: 1,
+    name: "I'm Todo Nº1",
+    description: 'Im the first todo',
+    priority: 'low',
+  },
+  {
+    id: 2,
+    name: "I'm Todo Nº2",
+    description: 'Im the second todo',
+    priority: 'low',
+  },
+  {
+    id: 3,
+    name: "I'm Todo Nº3",
+    description: 'Im the third todo',
+    priority: 'low',
+  },
+  {
+    id: 4,
+    name: "I'm Todo Nº4",
+    description: 'Im the fourth todo',
+    priority: 'low',
+  },
+];
