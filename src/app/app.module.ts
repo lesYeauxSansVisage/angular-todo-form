7a6c7adce0c10d1908d038ef08a89c0b737f71fa
Gen <genesishawkeye42@gmail.com>
2022-10-26 19:55:49 -0300

    added the todos list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodosFormComponent } from './components/todos-form/todos-form.component';
+import { TodosListComponent } from './components/todos-list/todos-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TodosFormComponent],
+  declarations: [AppComponent, TodosFormComponent, TodosListComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
