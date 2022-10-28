5bbde4a1a4c108ba093812da11928179143a523a
Gen <genesishawkeye42@gmail.com>
2022-10-27 22:53:53 -0300

    added the method to add todo

--- src/app/services/todos-service.service.ts
+++ src/app/services/todos-service.service.ts
@@ -8,10 +8,33 @@ import { mockTodos } from '../mock-todos';
 export class TodosServiceService {
   todos: Todo[] = mockTodos;
 
+  todo = {
+    id: undefined,
+    name: '',
+    description: '',
+    priority: '',
+  };
+
   constructor() {}
 
   getTodos(): Todo[] {
     return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    todo.id = this.todos.length + 1;
+    todo.name = todo.name;
+    todo.description = todo.description;
+    todo.priority = todo.priority;
+
+    this.todos.push(todo);
+
+    todo.id = 0;
+    todo.name = '';
+    todo.description = '';
+    todo.priority = '';
+
+    console.log(this.todos);
   }
 
   logTodos(): void {
