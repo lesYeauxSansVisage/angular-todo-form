5c47f5c56dc5240e87aef36848e956172224e8d5
Gen <genesishawkeye42@gmail.com>
2022-11-11 17:57:21 -0300

    added todos filter to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TodosFormComponent } from './components/todos-list/todos-form/todos-form.component';
 import { TodosListComponent } from './components/todos-list/todos-list.component';
 import { TodoItemComponent } from './components/todos-list/todo-item/todo-item.component';
+import { TodosFilterComponent } from './components/todos-list/todos-filter/todos-filter.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TodoItemComponent } from './components/todos-list/todo-item/todo-item.c
     TodosFormComponent,
     TodosListComponent,
     TodoItemComponent,
+    TodosFilterComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
